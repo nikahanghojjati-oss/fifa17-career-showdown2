f6c8ac6b228736212c301aadbe7b775415b39dab
nikahanghojjati-oss <nikahang.hojjati@gmail.com>
2026-08-02 04:13:23 -0400

    Update showdownUI.js

--- js/showdownUI.js
+++ js/showdownUI.js
@@ -1,1 +1,58 @@
+/* =====================================================
+   FIFA 17 Career Mode Showdown
 
+   Milestone 3
+
+   Showdown Interface Controller
+===================================================== */
+
+
+
+document.addEventListener(
+    "DOMContentLoaded",
+    () => {
+
+
+        initializeShowdownUI();
+
+
+    }
+);
+
+
+
+
+
+
+
+function initializeShowdownUI(){
+
+
+
+    const startButton =
+        document.getElementById(
+            "startShowdown"
+        );
+
+
+
+    if(startButton){
+
+
+        startButton.addEventListener(
+            "click",
+            () => {
+
+
+                createShowdown();
+
+
+            }
+        );
+
+
+    }
+
+
+
+}
