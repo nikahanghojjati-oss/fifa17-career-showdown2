fb7051eb0c8fa963169f862d3e9ee2ac3bde8eba
nikahanghojjati-oss <nikahang.hojjati@gmail.com>
2026-08-02 04:24:15 -0400

    Update leagues.js

--- data/leagues.js
+++ data/leagues.js
@@ -1,1 +1,65 @@
+/* =====================================================
+   FIFA 17 Career Mode Showdown
 
+   League Database
+
+===================================================== */
+
+
+const leagues = [
+
+{
+    id:"premier_league",
+
+    name:"Premier League",
+
+    country:"England",
+
+    logo:"assets/logos/premierleague.png"
+},
+
+
+{
+    id:"laliga",
+
+    name:"LaLiga",
+
+    country:"Spain",
+
+    logo:"assets/logos/laliga.png"
+},
+
+
+{
+    id:"bundesliga",
+
+    name:"Bundesliga",
+
+    country:"Germany",
+
+    logo:"assets/logos/bundesliga.png"
+},
+
+
+{
+    id:"serie_a",
+
+    name:"Serie A",
+
+    country:"Italy",
+
+    logo:"assets/logos/seriea.png"
+},
+
+
+{
+    id:"ligue_1",
+
+    name:"Ligue 1",
+
+    country:"France",
+
+    logo:"assets/logos/ligue1.png"
+}
+
+];
