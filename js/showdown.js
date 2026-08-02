59cdc8dfeb3249b969c37e3851cd537cbd5b34cc
nikahanghojjati-oss <nikahang.hojjati@gmail.com>
2026-08-02 04:08:53 -0400

    Update showdown.js

--- js/showdown.js
+++ js/showdown.js
@@ -1,1 +1,115 @@
+/* =====================================================
+   FIFA 17 Career Mode Showdown
+   Milestone 3
 
+   Showdown Manager
+===================================================== */
+
+
+
+let currentShowdown = null;
+
+
+
+
+
+function createShowdown(){
+
+
+
+    const showdownName =
+        document.getElementById(
+            "showdownName"
+        ).value;
+
+
+
+    const managerOne =
+        document.getElementById(
+            "managerOne"
+        ).value;
+
+
+
+    const managerTwo =
+        document.getElementById(
+            "managerTwo"
+        ).value;
+
+
+
+    const roundAmount =
+        document.getElementById(
+            "roundAmount"
+        ).value;
+
+
+
+
+
+    currentShowdown = {
+
+
+        id:
+        Date.now(),
+
+
+        name:
+        showdownName || "Unnamed Showdown",
+
+
+
+        managers:{
+
+
+            playerOne:
+            managerOne || "Manager 1",
+
+
+            playerTwo:
+            managerTwo || "Manager 2"
+
+
+        },
+
+
+
+        totalRounds:
+        Number(roundAmount),
+
+
+
+        currentRound:
+        1,
+
+
+
+        status:
+        "Created",
+
+
+
+        rounds:
+        []
+
+
+
+    };
+
+
+
+
+
+    console.log(
+        "Showdown Created:",
+        currentShowdown
+    );
+
+
+
+    showScreen(
+        "leagueWheelScreen"
+    );
+
+
+}
