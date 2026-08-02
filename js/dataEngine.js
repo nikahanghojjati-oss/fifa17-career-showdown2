635d8cd64d7c76368fd0e5eb33f76d84e5e605c3
nikahanghojjati-oss <nikahang.hojjati@gmail.com>
2026-08-02 04:26:20 -0400

    Update dataEngine.js

--- js/dataEngine.js
+++ js/dataEngine.js
@@ -1,1 +1,48 @@
+/* =====================================================
+   FIFA 17 Career Mode Showdown
 
+   Data Engine
+
+===================================================== */
+
+
+
+function getAllLeagues(){
+
+    return leagues;
+
+}
+
+
+
+
+
+function getLeagueById(id){
+
+
+
+    return leagues.find(
+        league => league.id === id
+    );
+
+
+}
+
+
+
+
+
+function getRandomLeague(){
+
+
+
+    const randomIndex =
+        Math.floor(
+            Math.random() * leagues.length
+        );
+
+
+
+    return leagues[randomIndex];
+
+}
