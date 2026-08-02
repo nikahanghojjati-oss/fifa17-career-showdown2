25ef8b5cba94d5743b0af3030b853d733fd80c8b
nikahanghojjati-oss <nikahang.hojjati@gmail.com>
2026-08-02 04:00:39 -0400

    Update screens.js

--- js/screens.js
+++ js/screens.js
@@ -1,1 +1,105 @@
+/* =====================================================
+   FIFA 17 Career Mode Showdown
+   Milestone 3
+   Screen Engine
+===================================================== */
+
+
+const screens = [
+
+    "mainMenu",
+
+    "createShowdown",
+
+    "leagueWheelScreen",
+
+    "clubWheelScreen",
+
+    "dashboard",
+
+    "legacy"
+
+];
+
+
+
+
+
+function showScreen(screenName){
+
+
+    screens.forEach(screen => {
+
+
+        const element = document.getElementById(screen);
+
+
+
+        if(element){
+
+
+            element.classList.add("hidden");
+
+
+        }
+
+
+    });
+
+
+
+    const activeScreen = document.getElementById(screenName);
+
+
+
+    if(activeScreen){
+
+
+        activeScreen.classList.remove("hidden");
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+function initializeScreens(){
+
+
+    const newShowdownButton = document.getElementById(
+        "newShowdown"
+    );
+
+
+
+    if(newShowdownButton){
+
+
+        newShowdownButton.addEventListener(
+            "click",
+            () => {
+
+
+                showScreen(
+                    "createShowdown"
+                );
+
+
+            }
+        );
+
+
+    }
+
+
+
+
+
+}
 
