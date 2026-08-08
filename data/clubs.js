60c529b7bf2ce50b895d0296836c14dbf175c56e
nikahanghojjati-oss <nikahang.hojjati@gmail.com>
2026-08-08 11:21:40 -0400

    Align club database keys with league IDs

--- data/clubs.js
+++ data/clubs.js
@@ -4,11 +4,11 @@
 ===================================================== */
 
 const clubsByLeague = {
-    premierLeague: ["Arsenal", "Bournemouth", "Burnley", "Chelsea", "Crystal Palace", "Everton", "Hull City", "Leicester City", "Liverpool", "Manchester City", "Manchester United", "Middlesbrough", "Southampton", "Stoke City", "Sunderland", "Swansea City", "Tottenham Hotspur", "Watford", "West Bromwich Albion", "West Ham United"],
-    laLiga: ["Alavés", "Athletic Club", "Atlético Madrid", "Barcelona", "Celta Vigo", "Deportivo La Coruña", "Eibar", "Espanyol", "Granada", "Las Palmas", "Leganés", "Málaga", "Osasuna", "Real Betis", "Real Madrid", "Real Sociedad", "Sevilla", "Sporting Gijón", "Valencia", "Villarreal"],
+    premier_league: ["Arsenal", "Bournemouth", "Burnley", "Chelsea", "Crystal Palace", "Everton", "Hull City", "Leicester City", "Liverpool", "Manchester City", "Manchester United", "Middlesbrough", "Southampton", "Stoke City", "Sunderland", "Swansea City", "Tottenham Hotspur", "Watford", "West Bromwich Albion", "West Ham United"],
+    laliga: ["Alavés", "Athletic Club", "Atlético Madrid", "Barcelona", "Celta Vigo", "Deportivo La Coruña", "Eibar", "Espanyol", "Granada", "Las Palmas", "Leganés", "Málaga", "Osasuna", "Real Betis", "Real Madrid", "Real Sociedad", "Sevilla", "Sporting Gijón", "Valencia", "Villarreal"],
     bundesliga: ["Bayern Munich", "Borussia Dortmund", "Bayer Leverkusen", "Borussia Mönchengladbach", "Schalke 04", "Mainz 05", "Hertha BSC", "Wolfsburg", "Hoffenheim", "Eintracht Frankfurt", "Werder Bremen", "Hamburg", "FC Augsburg", "SC Freiburg", "RB Leipzig", "FC Ingolstadt", "Darmstadt 98", "1. FC Köln"],
-    serieA: ["Atalanta", "Bologna", "Cagliari", "Chievo", "Crotone", "Empoli", "Fiorentina", "Genoa", "Inter Milan", "Juventus", "Lazio", "Milan", "Napoli", "Palermo", "Pescara", "Roma", "Sampdoria", "Sassuolo", "Torino", "Udinese"],
-    ligue1: ["Angers", "Bastia", "Bordeaux", "Caen", "Dijon", "Guingamp", "Lille", "Lorient", "Lyon", "Marseille", "Metz", "Monaco", "Montpellier", "Nancy", "Nantes", "Nice", "Paris Saint-Germain", "Rennes", "Saint-Étienne", "Toulouse"]
+    serie_a: ["Atalanta", "Bologna", "Cagliari", "Chievo", "Crotone", "Empoli", "Fiorentina", "Genoa", "Inter Milan", "Juventus", "Lazio", "Milan", "Napoli", "Palermo", "Pescara", "Roma", "Sampdoria", "Sassuolo", "Torino", "Udinese"],
+    ligue_1: ["Angers", "Bastia", "Bordeaux", "Caen", "Dijon", "Guingamp", "Lille", "Lorient", "Lyon", "Marseille", "Metz", "Monaco", "Montpellier", "Nancy", "Nantes", "Nice", "Paris Saint-Germain", "Rennes", "Saint-Étienne", "Toulouse"]
 };
 
 function getClubsForLeague(leagueId){
