ab6754e9b370c73bd3ea07e106aedb8a102ee0f4
nikahanghojjati-oss <nikahang.hojjati@gmail.com>
2026-09-04 22:35:28 -0400

    Roll whole shell forward to 1.9.1-r1

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const RUNTIME_REVISION = "1.9.0-r5";
-const PREVIOUS_RUNTIME_REVISION = "1.9.0-r4";
+const RUNTIME_REVISION = "1.9.1-r1";
+const PREVIOUS_RUNTIME_REVISION = "1.9.0-r5";
 const CACHE_PREFIX = "career-mode-showdown-shell-";
 const MODE_CACHE_PREFIX = "career-mode-showdown-runtime-mode-";
 const CACHE_NAME = `${CACHE_PREFIX}${RUNTIME_REVISION}`;
