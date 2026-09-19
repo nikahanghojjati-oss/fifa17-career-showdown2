1d424662a1eecea7c5763db746e440c75d82718f
nikahanghojjati-oss <nikahang.hojjati@gmail.com>
2026-09-19 16:52:45 -0400

    Promote runtime references to r34 in service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const RUNTIME_REVISION = "1.9.1-r33";
-const PREVIOUS_RUNTIME_REVISION = "1.9.1-r32";
+const RUNTIME_REVISION = "1.9.1-r34";
+const PREVIOUS_RUNTIME_REVISION = "1.9.1-r33";
 const CACHE_PREFIX = "career-mode-showdown-shell-";
 const MODE_CACHE_PREFIX = "career-mode-showdown-runtime-mode-";
 const CACHE_NAME = `${CACHE_PREFIX}${RUNTIME_REVISION}`;
