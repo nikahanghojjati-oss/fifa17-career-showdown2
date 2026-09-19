3651d9279a344fc0b5bb28662331274b05731bb4
nikahanghojjati-oss <nikahang.hojjati@gmail.com>
2026-09-19 12:56:00 -0400

    Promote Career Start recovery to r31

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const RUNTIME_REVISION = "1.9.1-r30";
-const PREVIOUS_RUNTIME_REVISION = "1.9.1-r29";
+const RUNTIME_REVISION = "1.9.1-r31";
+const PREVIOUS_RUNTIME_REVISION = "1.9.1-r30";
 const CACHE_PREFIX = "career-mode-showdown-shell-";
 const MODE_CACHE_PREFIX = "career-mode-showdown-runtime-mode-";
 const CACHE_NAME = `${CACHE_PREFIX}${RUNTIME_REVISION}`;
